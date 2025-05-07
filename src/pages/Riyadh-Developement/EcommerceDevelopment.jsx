ace8873b7e8aff671a0908b1f8028de6bf127313
Sandeep Kumar <sandeep.asvayuktech@gmail.com>
2025-05-07 18:23:00 +0530

    Pages creation Riyadh Developement

---
+++ src/pages/Riyadh-Developement/EcommerceDevelopment.jsx
@@ -0,0 +1,400 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Meta_Component from "../../Components/Meta_Component/Meta_Component";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import { TbShoppingCartSearch } from "react-icons/tb";
+
+const EcommerceDevelopment = () => {
+  return (
+    <>
+      <Meta_Component
+        title="mobile-app-development-company-riyadh-saudi-arabia"
+        description="Asvayuk Technologies, a top Website Development Company in Chandigarh & Mohali, offers custom web development, e-commerce solutions, & responsive designs."
+        canonical="https://asvayuktech.com/website-development-company-in-chandigarh-and-mohali"
+      />
+      <main>
+        {/* Banner Section */}
+        <section className="riyadhBanner-wrapper">
+          <Container>
+            <div className="riyadhBannerContent-wrapper">
+              <div className="riyadhBannerContent">
+                <h1 className="fw-bold riyadhBannerContent-title">
+                  <span>Ecommerce</span> Development <br /> Company in Saudi
+                  Arabia
+                </h1>
+                <p className="riyadhBannerContent-desc">
+                  Asvayuk is the best Ecommerce development company in Saudi
+                  Arabia providing top-notch Android and iOS app development
+                  services to accelerate your business success. Get in Touch!
+                </p>
+                <Link to="/contact-us" className="riyadhBtn">
+                  <div className="riyadhBtn-icons">
+                    <FaArrowRight />
+                  </div>
+                  <div className="riyadhBtn-title">Get In Touch</div>
+                </Link>
+              </div>
+              <Row className="riyadhRowMini-wrapper">
+                <Col lg={4}>
+                  <div className="riyadhMiniTitle-wrapper">
+                    <div className="titleMini-icon">
+                      <TbShoppingCartSearch />
+                    </div>
+                    <div className="titleMini-text">
+                      <small>
+                        Advanced AI-Powered <br /> App Solutions
+                      </small>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="riyadhMiniTitle-wrapper afterBorder">
+                    <div className="titleMini-icon">
+                      <TbShoppingCartSearch />
+                    </div>
+                    <div className="titleMini-text">
+                      <small>
+                        Agile Development for
+                        <br /> Quick Launch
+                      </small>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="riyadhMiniTitle-wrapper">
+                    <div className="titleMini-icon">
+                      <TbShoppingCartSearch />
+                    </div>
+                    <div className="titleMini-text">
+                      <small>
+                        Top-Rated App
+                        <br /> Developers Worldwide
+                      </small>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </section>
+
+        {/* Trusted Section */}
+        <section className="trusted-wrapper">
+          <Container>
+            <div className="trustedContent-wrapper">
+              <h2 className="mb-4">
+                Setting a New Standard in App
+                <br /> Development – Trusted by the Elite <br />
+                of Saudi Arabia!
+              </h2>
+              <Row className="trustedContent-Row">
+                <Col lg={3}>
+                  <div className="trustedDiv-wrapper">
+                    <div className="trustedDiv-first">
+                      <span>11+</span>
+                    </div>
+                    <div className="trustedDiv-second">
+                      <h5>Years of Experience</h5>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={3}>
+                  <div className="trustedDiv-wrapper">
+                    <div className="trustedDiv-first">
+                      <span>25+</span>
+                    </div>
+                    <div className="trustedDiv-second">
+                      <h5>Countries Served</h5>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={3}>
+                  <div className="trustedDiv-wrapper">
+                    <div className="trustedDiv-first">
+                      <span>250+</span>
+                    </div>
+                    <div className="trustedDiv-second">
+                      <h5>Tech Enthusiast</h5>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={3}>
+                  <div className="trustedDiv-wrapper">
+                    <div className="trustedDiv-first">
+                      <span>1000+</span>
+                    </div>
+                    <div className="trustedDiv-second">
+                      <h5>Products Delivered</h5>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </section>
+
+        {/* Experience Section */}
+        <section className="experience-wrapper">
+          <Container>
+            <div className="trustedContent-wrapper pt-2">
+              <h2 className="pb-3">
+                Experience Innovation with the Latest
+                <br />
+                Technologies Powering Your App Solutions
+              </h2>
+              <p className="fs-6">
+                You can hire a mobile app development company that uses the
+                latest technologies <br />
+                to help you embark on your app development journey.
+              </p>
+              <Row className="experienceWrapper-Row">
+                <Col lg={4}>
+                  <div className="experienceCard-wrapper">
+                    <div className="experienceBanner-Area">
+                      <div className="experienceBanner-Area1">
+                        <div className="experienceBannerArea1-Content">
+                          <div className="smallExperienceImage">
+                            <img
+                              src="https://d3l9a8mvoa6cl8.cloudfront.net/wp-content/uploads/2024/07/08182801/saudi_solution1.png"
+                              alt=""
+                              className="w-50"
+                            />
+                          </div>
+                          <h4>Artificial Intelligence</h4>
+                        </div>
+                      </div>
+                      <div className="experienceBanner-Area2 text-start">
+                        <p>
+                          AI can make your app experience much better by
+                          offering personalized content, smart predictions, and
+                          automated features. With AI integration, our app
+                          developer company ensures your app adapts to your
+                          users' changing needs, making it more user-friendly
+                          and efficient.
+                        </p>
+                        <ul className="ps-4">
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="experienceCard-wrapper">
+                    <div className="experienceBanner-Area">
+                      <div className="experienceBanner-Area1">
+                        <div className="experienceBannerArea1-Content">
+                          <div className="smallExperienceImage">
+                            <img
+                              src="https://d3l9a8mvoa6cl8.cloudfront.net/wp-content/uploads/2024/07/08182801/saudi_solution1.png"
+                              alt=""
+                              className="w-50"
+                            />
+                          </div>
+                          <h4>Artificial Intelligence</h4>
+                        </div>
+                      </div>
+                      <div className="experienceBanner-Area2 text-start">
+                        <p>
+                          AI can make your app experience much better by
+                          offering personalized content, smart predictions, and
+                          automated features. With AI integration, our app
+                          developer company ensures your app adapts to your
+                          users' changing needs, making it more user-friendly
+                          and efficient.
+                        </p>
+                        <ul className="ps-4">
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="experienceCard-wrapper">
+                    <div className="experienceBanner-Area">
+                      <div className="experienceBanner-Area1">
+                        <div className="experienceBannerArea1-Content">
+                          <div className="smallExperienceImage">
+                            <img
+                              src="https://d3l9a8mvoa6cl8.cloudfront.net/wp-content/uploads/2024/07/08182801/saudi_solution1.png"
+                              alt=""
+                              className="w-50"
+                            />
+                          </div>
+                          <h4>Artificial Intelligence</h4>
+                        </div>
+                      </div>
+                      <div className="experienceBanner-Area2 text-start">
+                        <p>
+                          AI can make your app experience much better by
+                          offering personalized content, smart predictions, and
+                          automated features. With AI integration, our app
+                          developer company ensures your app adapts to your
+                          users' changing needs, making it more user-friendly
+                          and efficient.
+                        </p>
+                        <ul className="ps-4">
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="experienceCard-wrapper">
+                    <div className="experienceBanner-Area">
+                      <div className="experienceBanner-Area1">
+                        <div className="experienceBannerArea1-Content">
+                          <div className="smallExperienceImage">
+                            <img
+                              src="https://d3l9a8mvoa6cl8.cloudfront.net/wp-content/uploads/2024/07/08182801/saudi_solution1.png"
+                              alt=""
+                              className="w-50"
+                            />
+                          </div>
+                          <h4>Artificial Intelligence</h4>
+                        </div>
+                      </div>
+                      <div className="experienceBanner-Area2 text-start">
+                        <p>
+                          AI can make your app experience much better by
+                          offering personalized content, smart predictions, and
+                          automated features. With AI integration, our app
+                          developer company ensures your app adapts to your
+                          users' changing needs, making it more user-friendly
+                          and efficient.
+                        </p>
+                        <ul className="ps-4">
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="experienceCard-wrapper">
+                    <div className="experienceBanner-Area">
+                      <div className="experienceBanner-Area1">
+                        <div className="experienceBannerArea1-Content">
+                          <div className="smallExperienceImage">
+                            <img
+                              src="https://d3l9a8mvoa6cl8.cloudfront.net/wp-content/uploads/2024/07/08182801/saudi_solution1.png"
+                              alt=""
+                              className="w-50"
+                            />
+                          </div>
+                          <h4>Artificial Intelligence</h4>
+                        </div>
+                      </div>
+                      <div className="experienceBanner-Area2 text-start">
+                        <p>
+                          AI can make your app experience much better by
+                          offering personalized content, smart predictions, and
+                          automated features. With AI integration, our app
+                          developer company ensures your app adapts to your
+                          users' changing needs, making it more user-friendly
+                          and efficient.
+                        </p>
+                        <ul className="ps-4">
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className="experienceCard-wrapper">
+                    <div className="experienceBanner-Area">
+                      <div className="experienceBanner-Area1">
+                        <div className="experienceBannerArea1-Content">
+                          <div className="smallExperienceImage">
+                            <img
+                              src="https://d3l9a8mvoa6cl8.cloudfront.net/wp-content/uploads/2024/07/08182801/saudi_solution1.png"
+                              alt=""
+                              className="w-50"
+                            />
+                          </div>
+                          <h4>Artificial Intelligence</h4>
+                        </div>
+                      </div>
+                      <div className="experienceBanner-Area2 text-start">
+                        <p>
+                          AI can make your app experience much better by
+                          offering personalized content, smart predictions, and
+                          automated features. With AI integration, our app
+                          developer company ensures your app adapts to your
+                          users' changing needs, making it more user-friendly
+                          and efficient.
+                        </p>
+                        <ul className="ps-4">
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                          <li className="pb-2">
+                            <p className="mb-0">Predictive Analytics</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <Link to="/contact-us" className="riyadhBtn text-white">
+                <div className="riyadhBtn-icons">
+                  <FaArrowRight />
+                </div>
+                <div className="riyadhBtn-title">Get In Touch</div>
+              </Link>
+            </div>
+          </Container>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default EcommerceDevelopment;
