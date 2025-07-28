4587d6a6ef920681e523cbfbfaf8070e35afdad2
sauravasvayuktech <saurav.asvayuk.tech@gmail.com>
2025-07-28 18:13:27 +0530

    optimization ongoing

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -107,7 +107,7 @@ function Home() {
                             </div>
                             <div className="col-lg-6">
                                 <div className="bannerImg">
-                                    <img src={bannerImg} alt="design-development-marketing" loading='lazy' width="100%" height="100%" />
+                                    <img src={bannerImg} alt="design-development-marketing" width="100%" height="100%" />
                                 </div>
                             </div>
                         </div>
